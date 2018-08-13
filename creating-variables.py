f91e0d3303f48f457165ea9fd6c2449c41161894
Administrator <admin@example.com>
2018-08-13 22:10:24 +0530

    myfirstcommit

---
+++ creating-variables.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# String Variable
+IntelHardWare='sony'
+OS="Windows"
+bit_Details="""64 Bit OS"""
+_Configuration007='''MS'''
+
+print(IntelHardWare,type(IntelHardWare),id(IntelHardWare))
+"""
+print(OS,type(OS))
+print(bit_Details,type(bit_Details))
+print(_Configuration007,type(_Configuration007))
+
+numbers=0
+num01=10.75
+
+print(numbers,type(numbers))
+print(num01,type(num01))
+
+aBookName='World'
+autherName="ayn rand"
+
+print(aBookName,type(aBookName))
+print(autherName,type(autherName))
+"""
---
+++ numbers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+#print("Welcome to Python World")
+#print("Day-3")
+#print('Today Topics is : Python Data Types i.e. 1. Numbers')
+
+# Comments are 5 Types :
+'1. Single-Line Comments  : 1. # , 2. '' & 3. " "'
+#2. Multi-Line Comments   : 1. ''' ''' & 2. """ """
+
+'''
+Rule of Creating Variables in Python:
+
+1. all lower case alphabates     : a-z
+2. all upper case alphabates     : A-Z
+3. combination of upper & lower  : A-Za-z
+4. Using numbers                 : 0-9
+5. Special char                  : _
+'''
+
+_='World is an idea'
+__=500
+
+print("Book Name is : ",_)
+print("")
+print("Price of the book is:",__)
+
+
+
