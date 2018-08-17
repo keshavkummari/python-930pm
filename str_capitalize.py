00f2dadd33b481dd44310be237684907fb5e97ee
keshavkummari <keshav.kummari@gmail.com>
2018-08-17 22:10:12 +0530

    str-capitalize-method

---
+++ str_capitalize.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# 1. capitalize() Method :
+
+# Note: string with only its first character capitalized.
+
+a01 = "welcome to python world"
+a02 = "python world"
+a03 = "to python world"
+
+print ("a01.capitalize() : ", a01.capitalize(),id(a01),type(a01),len(a01))
+print("")
+print ("a02.capitalize() : ", a02.capitalize(),id(a02),type(a02),len(a02))
+print("")
+print ("a03.capitalize() : ", a03.capitalize(),id(a03),type(a03),len(a03))
