19662f84d6577ad29562c1fd37e754951577976d
keshavkummari <keshav.kummari@gmail.com>
2018-08-17 22:29:04 +0530

    str-index-rindex-methods

---
+++ str_index-rindex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+str1 = "this is string example....wow!!!"
+str2 = "is"
+
+print (str1.rindex(str2))
+print (str1.index(str2))
+
+"""
+28	rindex(str, beg=0, end=len(string))
+	Same as index(), but search backwards in string.
+"""
+
